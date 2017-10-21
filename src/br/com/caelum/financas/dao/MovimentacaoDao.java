7bfcd93b769b2d7d88d12f212be16649ae4ec08f
Alessandro <ale.limaalexandre@gmail.com>
2017-10-21 14:31:38 -0200

    Exercicio 12.4 - Buscando movimentações seguindo alguns critérios

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -11,6 +11,7 @@ import javax.persistence.Query;
 import br.com.caelum.financas.exception.ValorInvalidoException;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 
 @Stateless
 public class MovimentacaoDao {
@@ -40,11 +41,22 @@ public class MovimentacaoDao {
 	}
 
 	public List<Movimentacao> listaTodasMovimentacoes(Conta conta) {
-		String jpql = "select m from Movimentacao m" 
-					+ " where m.conta = :conta order by m.valor desc";
+		String jpql = "select m from Movimentacao m" + ""
+				+ " where m.conta = :conta order by m.valor desc";
 		Query query = this.manager.createQuery(jpql);
 		query.setParameter("conta", conta);
+
+		return query.getResultList();
+	}
+
+	public List<Movimentacao> listaPorValorETipo(BigDecimal valor, TipoMovimentacao tipo) {
+		String jpql = "select m from Movimentacao m" 
+				+ " where m.valor <= :valor and m.tipoMovimentacao = :tipo";
+		Query query = this.manager.createQuery(jpql);
 		
+		query.setParameter("valor", valor);
+		query.setParameter("tipo", tipo);
+
 		return query.getResultList();
 	}
 
--- src/br/com/caelum/financas/mb/MovimentacoesPorValorETipoBean.java
+++ src/br/com/caelum/financas/mb/MovimentacoesPorValorETipoBean.java
@@ -2,16 +2,21 @@ package br.com.caelum.financas.mb;
 
 import java.math.BigDecimal;
 import java.util.List;
-import br.com.caelum.financas.modelo.Movimentacao;
-import br.com.caelum.financas.modelo.TipoMovimentacao;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.caelum.financas.dao.MovimentacaoDao;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 
 @Named
 @RequestScoped
 public class MovimentacoesPorValorETipoBean {
 	
+	@Inject
+	private MovimentacaoDao movimentacaoDao;
 	private List<Movimentacao> movimentacoes;
 	private BigDecimal valor;
 	private TipoMovimentacao tipoMovimentacao;
@@ -19,6 +24,8 @@ public class MovimentacoesPorValorETipoBean {
 
 	public void lista() {
 		System.out.println("Buscando movimentacoes por valor e tipo");
+		
+		movimentacoes = movimentacaoDao.listaPorValorETipo(valor, tipoMovimentacao);
 	}
 
 	public BigDecimal getValor() {

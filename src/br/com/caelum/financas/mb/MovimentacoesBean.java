f21cb83c0053227bcc15423d86d31cf664ccd529
Alessandro <ale.limaalexandre@gmail.com>
2017-10-28 12:07:50 -0200

    Exercicio 13.13 - Queries Planejadas (Continuação)

--- src/br/com/caelum/financas/mb/MovimentacoesBean.java
+++ src/br/com/caelum/financas/mb/MovimentacoesBean.java
@@ -25,18 +25,16 @@ public class MovimentacoesBean implements Serializable {
 
 	@Inject
 	private MovimentacaoDao movimentacaoDao;
-	
+
 	@Inject
 	private CategoriaDao categoriaDao;
-	
+
 	private static final long serialVersionUID = 1L;
 	private List<Movimentacao> movimentacoes;
 	private List<Categoria> categorias;
 	private Movimentacao movimentacao = new Movimentacao();
 	private Integer contaId;
 	private Integer categoriaId;
-	
-	
 
 	public void grava() {
 		System.out.println("Fazendo a gravacao da movimentacao");
@@ -84,9 +82,9 @@ public class MovimentacoesBean implements Serializable {
 	public void setCategoriaId(Integer categoriaId) {
 		this.categoriaId = categoriaId;
 	}
-	
+
 	public List<Categoria> getCategorias() {
-		if(this.categorias == null) {
+		if (this.categorias == null) {
 			System.out.println("Listando as categorias");
 			this.categorias = this.categoriaDao.lista();
 		}
@@ -104,9 +102,9 @@ public class MovimentacoesBean implements Serializable {
 	public TipoMovimentacao[] getTiposDeMovimentacao() {
 		return TipoMovimentacao.values();
 	}
-	
+
 	public void adicionaCategoria() {
-		if(this.categoriaId != null && this.categoriaId > 0) {
+		if (this.categoriaId != null && this.categoriaId > 0) {
 			Categoria categoria = categoriaDao.procura(this.categoriaId);
 			this.movimentacao.getCategorias().add(categoria);
 		}
